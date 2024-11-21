21cefd15d85a1c5de62ce64e00b3008f155cbcba
FAHMI AL-ANSHORI <139199297+FAHMIALAN@users.noreply.github.com>
2024-11-22 00:02:06 +0700

    CarUiState.kt

--- app/src/main/java/com/example/showroomjdm/ui/theme/CarUiState.kt
+++ app/src/main/java/com/example/showroomjdm/ui/theme/CarUiState.kt
@@ -1,6 +1,12 @@
 package com.example.showroomjdm.data
+// Menyatakan bahwa file ini berada di dalam package com.example.showroomjdm.data
 
 data class CarUiState(
+    // Mendefinisikan data class CarUiState yang digunakan untuk menyimpan dan mengelola state UI untuk daftar mobil dan favorit
+
     val cars: List<Car> = emptyList(),
+    // Properti 'cars' berfungsi untuk menyimpan daftar mobil. Nilainya berupa List<Car> yang berisi objek-objek mobil. Default-nya adalah list kosong (emptyList())
+
     val favoriteCars: List<Car> = emptyList()
+    // Properti 'favoriteCars' berfungsi untuk menyimpan daftar mobil yang difavoritkan. Nilainya berupa List<Car> yang berisi objek-objek mobil yang difavoritkan. Default-nya adalah list kosong (emptyList())
 )
